48f5e2e84eed6336d12ad1176dc56d73c8a2d431
fortune <omorodion.fortune@gmail.com>
2022-02-16 11:41:40 +0100

    modify googleAuth directory

--- Documents/Streams/client/src/components/GoogleAuth.js
+++ Documents/Streams/client/src/components/GoogleAuth.js
@@ -2,7 +2,13 @@ import React from "react";
 
 class GoogleAuth extends React.Component {
   componentDidMount() {
-    window.gapi.load("client:auth2");
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client.init({
+        clientId:
+          "192892251523-bncpabckgs8lnpvj95rcmaelujkj8u4p.apps.googleusercontent.com",
+        scope: "email",
+      });
+    });
   }
   render() {
     return <div>Google Auth</div>;
